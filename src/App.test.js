0c515306938956f06f62849615e9be703c6d060b
Rickard Jonsson <rickaard@gmail.com>
2020-10-20 07:49:33 +0200

    first init

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = getByText(/HellooOO/i);
+  expect(text).toBeInTheDocument();
 });
---
+++ src/game-utils/board.js
@@ -0,0 +1,36 @@
+export const board = [
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+];
+
+export const isHorizontalWin = (board, player) => {
+    const copiedBoard = [...board];
+    let isWin = false;
+    copiedBoard.forEach(row => {
+        let amountInRow = 0;
+        for (let cell of row) {
+            if (cell === player) {
+                amountInRow += 1;
+            } else {
+                amountInRow = 0;
+            }
+            if (amountInRow === 5) {
+                isWin = true;
+                break;
+            };
+        }
+    })
+
+    return isWin;
+};
+
+export const isVerticalWin = (board, player) => {
+    const copiedBoard = [...board];
+    // loop over copiedBoard
+    
+    // 
+};
