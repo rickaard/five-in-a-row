a8be395dde5972be70a2b6e6d0cfddd6849bc1a9
Rickard Jonsson <rickaard@gmail.com>
2020-10-21 07:47:04 +0200

    add tests for vertical win

--- src/utils/game-logic/board.js
+++ src/utils/game-logic/board.js
@@ -28,9 +28,20 @@ export const isHorizontalWin = (board, player) => {
     return isWin;
 };
 
+
 export const isVerticalWin = (board, player) => {
     const copiedBoard = [...board];
-    // loop over copiedBoard
+    let isWin = false;
     
-    // 
+    // convert to horizontal
+    copiedBoard.forEach(column => {
+        
+    })
+    
+    return true;
+};
+
+export const isDiagonalWin = (board, player) => {
+    const copiedBoard = [...board];
+    return true;
 };
---
+++ src/utils/game-logic/board.test.js
@@ -0,0 +1,133 @@
+import { isHorizontalWin, isVerticalWin } from './board';
+
+describe("Horizontal", () => {
+
+    describe("Wins", () => {
+        test("returns true if it's a win #1", () => {
+            const board = [
+                ['', 'x', 'x', '', 'x', 'x', 'x'],
+                ['', '', '', '', '', '', ''],
+                ['x', 'x', 'x', 'x', 'x', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+            ];
+            expect(isHorizontalWin(board, 'x')).toBe(true);
+        })
+
+        test("returns true if it's a win #2", () => {
+            const board = [
+                ['', 'x', 'x', '', 'x', 'x', 'x'],
+                ['', '', '', '', '', '', ''],
+                ['x', 'x', 'x', 'x', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', 'o', 'o', 'o', 'o', 'o'],
+                ['', '', '', '', '', '', ''],
+            ];
+            expect(isHorizontalWin(board, 'o')).toBe(true);
+        })
+
+        test("returns true if it's a win #3", () => {
+            const board = [
+                ['', 'x', 'x', '', 'x', 'x', 'x'],
+                ['', '', '', '', '', '', ''],
+                ['x', 'x', '', 'x', 'x', 'x', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', 'x', 'x', 'x', 'x', 'x', ''],
+            ];
+            expect(isHorizontalWin(board, 'x')).toBe(true);
+        })
+        test("returns true if it's a win #4", () => {
+            const board = [
+                ['', '', 'x', 'x', 'x', 'x', 'x'],
+                ['', '', '', '', '', '', ''],
+                ['x', 'x', '', 'x', 'x', 'x', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', 'x', 'x', 'x', '', 'x', ''],
+            ];
+            expect(isHorizontalWin(board, 'x')).toBe(true);
+        })
+    })
+
+    describe("Loss", () => {
+
+        test("returns false if it's not a win #1 - only 4 in a row", () => {
+            const board = [
+                ['', 'x', 'x', '', 'x', 'x', 'x'],
+                ['', '', '', '', '', '', ''],
+                ['x', 'x', '', 'x', 'x', 'x', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', 'x', 'x', 'x', 'x', ''],
+            ];
+            expect(isHorizontalWin(board, 'x')).toBe(false);
+        });
+
+        test("returns false if it's not a win #2 - not 5 in a row horizontal but vertical", () => {
+            const board = [
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+            ];
+            expect(isHorizontalWin(board, 'x')).toBe(false);
+        });
+
+        test("returns false if it's not a win #3 - not 5 in row anywhere", () => {
+            const board = [
+                ['x', 'x', 'x', 'x', 'o', 'x', 'x'],
+                ['x', 'x', 'o', 'x', 'x', 'x', 'x'],
+                ['x', 'x', 'x', 'o', 'x', 'x', 'x'],
+                ['x', 'o', 'x', 'x', 'x', 'x', 'o'],
+                ['x', 'x', 'x', 'x', 'o', 'x', 'x'],
+                ['o', 'o', 'o', 'o', 'x', 'x', 'x'],
+            ];
+            expect(isHorizontalWin(board, 'x')).toBe(false);
+        });
+    })
+})
+
+describe("Vertical", () => {
+
+    describe("Wins", () => {
+
+        test("Returns true #1", () => {
+            const board = [
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+            ];
+            expect(isVerticalWin(board)).toBe(true)
+        })
+        test("Returns true #2", () => {
+            const board = [
+                ['', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+                ['x', '', '', '', '', '', ''],
+            ];
+            expect(isVerticalWin(board)).toBe(true)
+        })
+        test("Returns true #3", () => {
+            const board = [
+                ['', '', '', '', 'x', '', ''],
+                ['x', '', '', '', 'x', '', ''],
+                ['x', '', '', '', 'x', '', ''],
+                ['x', '', '', '', 'x', '', ''],
+                ['x', '', '', '', 'x', '', ''],
+                ['', '', '', '', '', '', ''],
+            ];
+            expect(isVerticalWin(board)).toBe(true)
+        })
+    })
+
+})
